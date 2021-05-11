b52d779b772f708356ccbeb74cc13cd903f70624
Julian Estefan <julianestefan@MacBook-Pro-de-Julian.local>
2021-05-11 11:50:40 -0300

    begining exersice 2

---
+++ exersice2/practica.js
@@ -0,0 +1,22 @@
+let free = false
+const validClient = time => {
+    letage = prompt('¿cual es tu edad?');
+    
+    if (age > 18) {
+        if (time <= 2 && time >= 7 && free == false) {
+            alert("podes pasar gratis despues de las 2 am");
+            free = true
+        } else {
+            alert = ('podes pasar pero tenes que pagar la entrada');
+        }
+    } else {
+        alert = ('younger 18');
+    }
+}
+
+validClient(23);
+validClient(0.3);
+validClient(3);
+validClient(0.4);
+validClient(1);
+validClient(2.30);
