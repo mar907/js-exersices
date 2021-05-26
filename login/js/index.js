0400607efd3bc4731d2985129c6214b5f8bc76d1
Your Name <you@example.com>
2021-05-26 16:32:32 -0500

    cambiando el login

--- login/js/index.js
+++ login/js/index.js
@@ -1,26 +1,43 @@
-const usuarios = [
-    {
-        name: "Maria Veronica Fernandez",
-        email: "maria_veronica_f@outlook.com",
-        password: "tiagoivan"
-    }
-]
 
-function validarImput(){
-    respuesta= prompt("")
+const form= document.getElementsByTagName("login_form")[0];
+const email= document.getElementsById("email");
+const emailError= document.querySelector("#email + span.error");
+const password= document.getElementsById("contraseña");
+const passwordError= document.querySelector("#password + span.error");
+const boton= document.getElementsById("btn-enviar")
+
+email.addEventListener( 'input', function (event){
+if(email.validity.valid){
+emailError.innerHTML= '';
+emailError.className= 'error';
+}else{
+    showError();
+    debugger
 }
-document.getElementById("login_form").addEventListener(
-    "submit",
-    event => {
-        event.preventDefault()
-        const email = event.target.children.namedItem("email").value
-        const password = event.target.children.namedItem("password").value
-        //debugger
-        const user = usuarios.find( item => item.email  === email  )
-        const user = usuarios.find( item => item.password  === password  )
-       
-        console.log(user)
+});
+form.addEventListener( 'submit',function(event){
+    if(!email.validity.valid){
+        showError();
+        event.preventDefault();
     }
-) 
+});
+debugger
+ function showError(){
+    if(email.validity.valueMissing){
+        emailError.textContent=  'El correo es requerido';
 
-
+   }else if(email.validity.typeMismatch){
+            emailError.textContent= 'El correo no existe';
+        }
+        emailError.className= 'error activo';
+    };
+function showError(){
+    if(password.validity.valueMissing){
+        passwordError.textContent= 'La contraseña es requerida';
+    
+    }else if(password.validity.typeMismatch){
+        passwordError.textContent= 'Contraseña incorrecta';
+        debugger
+    }
+    passwordError.className= 'error activo';
+}
