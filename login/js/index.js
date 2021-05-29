4afc9a53186b789a8499cc99c5c243f4b7869f35
Veronica Fernandez <mariaveronicafernandez4@gmail.com>
2021-05-29 14:41:03 -0300

    cambios

--- login/js/index.js
+++ login/js/index.js
@@ -1,43 +1,47 @@
 
-const form= document.getElementsByTagName("login_form")[0];
-const email= document.getElementsById("email");
-const emailError= document.querySelector("#email + span.error");
-const password= document.getElementsById("contraseña");
-const passwordError= document.querySelector("#password + span.error");
-const boton= document.getElementsById("btn-enviar")
+const form = document.getElementById ("login_form");
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+const password = document.getElementById("contraseña");
+const passwordError = document.querySelector("#password + span.error");
+const boton = document.getElementById("btn-enviar")
 
-email.addEventListener( 'input', function (event){
-if(email.validity.valid){
-emailError.innerHTML= '';
-emailError.className= 'error';
-}else{
-    showError();
-    debugger
-}
+email.addEventListener('change', function (event) {
+
+    if (email.validity.valid) {
+        emailError.innerHTML = '';
+        emailError.className = 'error';
+    } else {
+        debugger
+        showError();
+    }
 });
-form.addEventListener( 'submit',function(event){
-    if(!email.validity.valid){
+
+form.addEventListener('submit', function (event) {
+    if (!email.validity.valid) {
         showError();
         event.preventDefault();
     }
 });
-debugger
- function showError(){
-    if(email.validity.valueMissing){
-        emailError.textContent=  'El correo es requerido';
 
-   }else if(email.validity.typeMismatch){
-            emailError.textContent= 'El correo no existe';
-        }
-        emailError.className= 'error activo';
-    };
-function showError(){
-    if(password.validity.valueMissing){
-        passwordError.textContent= 'La contraseña es requerida';
-    
-    }else if(password.validity.typeMismatch){
-        passwordError.textContent= 'Contraseña incorrecta';
+function showError() {
+    debugger
+    if (email.validity.valueMissing) {
+        emailError.textContent = 'El correo es requerido';
+
+    } else if (email.validity.typeMismatch) {
+        emailError.textContent = 'El correo no existe';
+    }
+    emailError.className = 'error activo';
+};
+
+function showError() {
+    if (password.validity.valueMissing) {
+        passwordError.textContent = 'La contraseña es requerida';
+
+    } else if (password.validity.typeMismatch) {
+        passwordError.textContent = 'Contraseña incorrecta';
         debugger
     }
-    passwordError.className= 'error activo';
+    passwordError.className = 'error activo';
 }
